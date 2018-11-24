274b829583e4f2c9b39057526b0a86a31bacd146
Raban von Spiegel <raban.spiegel@tum.de>
2018-11-23 22:25:33 -0500

    import cropper.js / clean up css

--- js/action.js
+++ js/action.js
@@ -29,6 +29,27 @@ var currentBorder;
 var currentHighlight = "#ff6666";
 
 
+import Cropper as 'cropperjs';
+
+const image = document.getElementById('image');
+const cropper = new Cropper(image, {
+  aspectRatio: 16 / 9,
+  crop(event) {
+    console.log(event.detail.x);
+    console.log(event.detail.y);
+    console.log(event.detail.width);
+    console.log(event.detail.height);
+    console.log(event.detail.rotate);
+    console.log(event.detail.scaleX);
+    console.log(event.detail.scaleY);
+  },
+});
+
+
+
+
+
+
 $(function(){
 
 	$('#worldBtn').on('click', function(){
